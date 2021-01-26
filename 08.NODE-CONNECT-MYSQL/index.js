8d0cef7d9e8696ec56eb3f51c03051f7ed51daf6
张国龙 <m2maomao@163.com>
2021-01-26 11:05:18 +0800

    +mysql

--- 08.NODE-CONNECT-MYSQL/index.js
+++ 08.NODE-CONNECT-MYSQL/index.js
@@ -2,18 +2,18 @@ const mysql = require('mysql');
 
 // 创建连接对象
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password:'123456',
   port:3306,
-  database: 'myblog'
+  database: 'new_demo1'
 });
 
 // 开始连接
 connection.connect();
 
 // 执行sql语句
-const sql = `select * from blogs`;
+const sql = `select * from new_table`;
 connection.query(sql, (err, result) => {
   if (err) {
     console.error('error', err);
